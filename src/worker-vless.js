f66a5d9489b483bf396f7c06868c60e77526bf16
Hanpiil <qwe452647338@gmail.com>
2025-03-29 19:01:27 +0800

    WD.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '81bef3ed-f322-4332-a351-1a12f4dd1398';
 
-let proxyIP = '';
+let proxyIP = 'acsg3.cloudflarest.link';
 
 
 if (!isValidUUID(userID)) {
